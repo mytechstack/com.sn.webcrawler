cf067a25e3b0a599fd5f5f12bdac802246f29836
Shridhar Nayak <shridhar.nayak@personalis.com>
2019-07-03 11:34:45 -0700

    add test case for search

--- src/test/java/com/sn/webcrawler/TestApplication.java
+++ src/test/java/com/sn/webcrawler/TestApplication.java
@@ -18,7 +18,7 @@ import junit.framework.TestCase;
 public class TestApplication extends TestCase {
 
 	SearchUtil searchUtil;
-	
+
 	@Test
 	public void test_getListofUrls() {
 		List<String> results = SearchUtil.getListOfSearchUrls();
@@ -26,14 +26,14 @@ public class TestApplication extends TestCase {
 		assertTrue(results.size() != 0);
 		assertEquals(results.get(0),"facebook.com/");
 	}
-	
+
 	@Test
 	public void test_getDocument() {
 		Document doc = SearchUtil.pageFetcher("facebook.com/");
 		assertNotNull(doc);
 		assertNotNull(doc.body());
 	}
-	
+
 	@Test
 	public void test_ProducerConsumer() throws InterruptedException {
 		String url="facebook.com/";
@@ -47,5 +47,14 @@ public class TestApplication extends TestCase {
 		Thread.sleep(1000);
 		assertEquals(queue.size(), 0);
 	}
-	
+
+	@Test
+	public void test_search() {
+		String result = SearchUtil.searchDocument("cloud business xyz business blogs", "facebook.com/");
+		System.out.println(result);
+		assertNotNull(result);
+		assertTrue(result.contains("Search term Business occurred 2 times in facebook.com"));
+		assertTrue(result.contains("Search term Blogs occurred 1 times in facebook.com/"));
+	}
+
 }
